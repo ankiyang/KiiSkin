e954cff2a2f62389c609b75449455afd8789454d
ankiyang <ankiyang1201@gmail.com>
2019-10-15 15:23:04 +1300

    opening page and initial signin page

--- app/src/main/java/com/example/kiiskin/MainActivity.kt
+++ app/src/main/java/com/example/kiiskin/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.kiiskin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) // direct to acitivity_main.xml
+
+        startButton.setOnClickListener {
+            val message: String = UserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+
+        }
+//        startButton.setOnClickListener {
+//            // Code
+//            Log.i("MainActivity", "Button was clicked")
+//            Toast.makeText(this, "Button was clicked", Toast.LENGTH_SHORT).show()
+//        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/kiiskin/SignIn.kt
@@ -0,0 +1,15 @@
+package com.example.kiiskin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class SignIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_signin)
+    }
+}
